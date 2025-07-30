98180e1d6c56cef7e42f82da7e7d1bf4707f0b97
Kelvin Gabriel Ribeiro <58612490+kelvingrib02@users.noreply.github.com>
2025-07-29 22:32:41 -0300

    Construindo home

--- Data/EcommerceContext.cs
+++ Data/EcommerceContext.cs
@@ -1,6 +1,14 @@
-﻿namespace EcommerceAPI.Data
+﻿using EcommerceAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class EcommerceContext : DbContext
 {
-    public class EcommerceContext
-    {
-    }
+    public EcommerceContext(DbContextOptions<EcommerceContext> options)
+        : base(options) { }
+
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Imovel> Imoveis { get; set; }
+    public DbSet<Pedido> Pedidos { get; set; }
+    public DbSet<ItemPedido> ItensPedido { get; set; }
+    public DbSet<ImagemImovel> ImagensImovel { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -1,51 +1,27 @@
-using Microsoft.EntityFrameworkCore;
-using EcommerceAPI.Data; 
+﻿using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Registra os serviços necessários
+builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<EcommerceContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configura o pipeline de requisições
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // opcional: exibe erros detalhados no navegador
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles(); // se for usar CSS, JS, imagens
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
